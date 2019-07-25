518118d03a0a04681a54e970a8449b88d9385f22
David Collins <david.collins2@hdr.mq.edu.au>
2019-07-25 16:07:23 +1000

    Test vector class when typing mixed types

--- scripts/Introduction.R
+++ scripts/Introduction.R
@@ -14,3 +14,22 @@ width <- 50
 area <- length * width
 area
 
+b <- sqrt(a)
+
+no_membrs <- c(3,7,10,6) # "c" is to combine into a vector or concatenate
+length(no_membrs)
+class(no_membrs)
+str(no_membrs) # str = structure
+
+respondent_wall_type <- c("mud", "brick", "wood")
+length(respondent_wall_type)
+class(respondent_wall_type)
+str(respondent_wall_type)
+
+possessions <- c("bike", "radio", "TV")
+possessions <- c(possessions, "phone") # add another element to vector
+possessions <- c("car", possessions) 
+
+
+
+
