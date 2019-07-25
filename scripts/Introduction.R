95af47408b16516dcd4a270b59978404990288ac
David Collins <david.collins2@hdr.mq.edu.au>
2019-07-25 15:38:58 +1000

    Assign value to area hectares and excerice

--- scripts/Introduction.R
+++ scripts/Introduction.R
@@ -3,3 +3,14 @@
 # library(tidyverse)
 
 print("Hello world")
+
+area_hectares <- 2.5                # land area in hectares
+area_acres <- 2.47 * area_hectares  #convert to acres
+area_acres                          # print
+
+# Excercise - changes not made until you run the full script
+length <- 5
+width <- 50
+area <- length * width
+area
+
