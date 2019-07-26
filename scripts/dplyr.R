b615b55cef42be70d7a7ce22b83b2aeceb6ef739
David Collins <david.collins2@hdr.mq.edu.au>
2019-07-26 13:39:08 +1000

    Muate added

--- scripts/dplyr.R
+++ scripts/dplyr.R
@@ -6,6 +6,22 @@ interviews <- read_csv("data/SAFI_clean.csv", na="NULL")
 select(interviews, village, no_membrs, years_liv)
 filter(interviews, village == "God", no_membrs >6)
 
+# Excercise
 interviews %>%
   filter(memb_assoc == "yes") %>%
   select(affect_conflicts, liv_count, no_meals)
+
+# Mutate
+interviews %>%
+  mutate(people_per_room = no_membrs / rooms)
+
+interviews %>%
+  filter(!is.na(memb_assoc)) %>%
+  mutate(people_per_room = no_membrs / rooms)
+
+interviews_total_meals <- interviews %>%
+  mutate(total_meals = no_membrs * no_meals) %>%
+  filter(total_meals > 20) %>%
+  select(village, total_meals)
+
+interviews_total_meals
