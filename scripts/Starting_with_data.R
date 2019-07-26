86fd3f8b90c7ef466d21e680e2edbbf7bc792925
David Collins <david.collins2@hdr.mq.edu.au>
2019-07-26 11:50:15 +1000

    Using factors to order

--- scripts/Starting_with_data.R
+++ scripts/Starting_with_data.R
@@ -35,3 +35,24 @@ interviews_last <- interviews[n_row,]
 interviews_last
 ceiling(67)
 interviews
+
+affect_conflicts <- interviews$affect_conflicts
+affect_conflicts <- as.factor(affect_conflicts)
+affect_conflicts
+plot(affect_conflicts)
+
+## Let's recreate the vector from the data frame column "affect_conflicts"
+affect_conflicts <- interviews$affect_conflicts
+## replace the missing data with "undetermined"
+affect_conflicts[is.na(affect_conflicts)] <- "undetermined"
+## convert it into a factor
+affect_conflicts <- as.factor(affect_conflicts)
+## let's see what it looks like
+affect_conflicts
+
+levels(affect_conflicts)[2] <- "more than once"
+affect_conflicts
+plot(affect_conflicts)
+
+affect_conflicts <- factor(affect_conflicts, levels = c("never", "once", "more than once", "frequently", "undetermined"))
+plot(affect_conflicts)
